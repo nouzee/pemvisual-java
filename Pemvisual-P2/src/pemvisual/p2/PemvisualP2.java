44d1b82ee3e6367c7911ab81d38360db00d70673
raihanalifp <132416154+nouzee@users.noreply.github.com>
2024-09-04 08:29:40 +0700

    Add files via upload

---
+++ Pemvisual-P2/src/pemvisual/p2/PemvisualP2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pemvisual.p2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class PemvisualP2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
